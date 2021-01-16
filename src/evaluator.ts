650afdba59422fa75ca3adb768afda7648087397
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2021-01-15 17:07:07 -0800

    Evaluate all the operators we can

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -1,6 +1,12 @@
 import * as ast from "./ast";
 import * as o from "./object";
 
+const NULL = new o.MonkeyNull();
+const TRUE = new o.MonkeyBoolean(true);
+const FALSE = new o.MonkeyBoolean(false);
+
+const objectifyBoolean = (b: boolean): o.MonkeyBoolean => (b ? TRUE : FALSE);
+
 const monkeyEval = (node: ast.Node): o.MonkeyObject => {
   if (node instanceof ast.Program) {
     return evalStatements(node.statements);
@@ -11,12 +17,156 @@ const monkeyEval = (node: ast.Node): o.MonkeyObject => {
   if (node instanceof ast.IntegerLiteral) {
     return new o.MonkeyInteger(node.value);
   }
+  if (node instanceof ast.BooleanLiteral) {
+    return objectifyBoolean(node.value);
+  }
+  if (node instanceof ast.PrefixExpression) {
+    const right = monkeyEval(node.right);
+    return evalPrefixExpression(node.operator, right);
+  }
+  if (node instanceof ast.InfixExpression) {
+    const left = monkeyEval(node.left);
+    const right = monkeyEval(node.right);
+    return evalInfixExpression(left, node.operator, right);
+  }
 
-  return new o.MonkeyNull();
+  return NULL;
 };
 
 const evalStatements = (statements: ast.Statement[]): o.MonkeyObject => {
-  return statements.reduce((_, stmt) => monkeyEval(stmt), new o.MonkeyNull());
+  return statements.reduce((_, stmt) => monkeyEval(stmt), NULL);
+};
+
+interface UnaryEvaluator {
+  (operand: o.MonkeyObject): o.MonkeyObject;
+}
+
+const UNARY_OPERATORS: Map<string, UnaryEvaluator> = new Map([
+  [
+    "!",
+    (operand: o.MonkeyObject): o.MonkeyObject => {
+      if (operand === FALSE || operand === NULL) {
+        return TRUE;
+      }
+      return FALSE;
+    },
+  ],
+  [
+    "-",
+    (operand: o.MonkeyObject): o.MonkeyObject => {
+      if (!(operand instanceof o.MonkeyInteger)) {
+        return NULL;
+      }
+      return new o.MonkeyInteger(-operand.value);
+    },
+  ],
+]);
+
+const evalPrefixExpression = (
+  operator: string,
+  right: o.MonkeyObject
+): o.MonkeyObject => {
+  const evaluator = UNARY_OPERATORS.get(operator);
+  if (!evaluator) {
+    return NULL;
+  }
+  return evaluator(right);
+};
+
+interface BinaryEvaluator {
+  (left: o.MonkeyObject, right: o.MonkeyObject): o.MonkeyObject;
+}
+
+interface BinaryNumericEvaluator {
+  (left: number, right: number): number;
+}
+
+const integerOperation = (f: BinaryNumericEvaluator) => (
+  left: o.MonkeyObject,
+  right: o.MonkeyObject
+): o.MonkeyObject => {
+  if (!(left instanceof o.MonkeyInteger && right instanceof o.MonkeyInteger)) {
+    return NULL;
+  }
+  return new o.MonkeyInteger(f(left.value, right.value));
+};
+
+interface BinaryNumericComparator {
+  (left: number, right: number): boolean;
+}
+
+const integerCompareOperation = (f: BinaryNumericComparator) => (
+  left: o.MonkeyObject,
+  right: o.MonkeyObject
+): o.MonkeyObject => {
+  if (!(left instanceof o.MonkeyInteger && right instanceof o.MonkeyInteger)) {
+    return NULL;
+  }
+  return objectifyBoolean(f(left.value, right.value));
+};
+
+const BINARY_OPERATORS: Map<string, BinaryEvaluator> = new Map([
+  [
+    "+",
+    integerOperation((left: number, right: number): number => left + right),
+  ],
+  [
+    "-",
+    integerOperation((left: number, right: number): number => left - right),
+  ],
+  [
+    "*",
+    integerOperation((left: number, right: number): number => left * right),
+  ],
+  [
+    "/",
+    integerOperation((left: number, right: number): number => left / right),
+  ],
+  [
+    "<",
+    integerCompareOperation(
+      (left: number, right: number): boolean => left < right
+    ),
+  ],
+  [
+    ">",
+    integerCompareOperation(
+      (left: number, right: number): boolean => left > right
+    ),
+  ],
+  [
+    "==",
+    (left: o.MonkeyObject, right: o.MonkeyObject): o.MonkeyObject => {
+      if (left instanceof o.MonkeyInteger && right instanceof o.MonkeyInteger) {
+        return objectifyBoolean(left.value === right.value);
+      }
+      if (left instanceof o.MonkeyBoolean && right instanceof o.MonkeyBoolean) {
+        return objectifyBoolean(left === right);
+      }
+      return NULL;
+    },
+  ],
+  [
+    "!=",
+    (left: o.MonkeyObject, right: o.MonkeyObject): o.MonkeyObject => {
+      if (left instanceof o.MonkeyInteger && right instanceof o.MonkeyInteger) {
+        return objectifyBoolean(left.value !== right.value);
+      }
+      return objectifyBoolean(left !== right);
+    },
+  ],
+]);
+
+const evalInfixExpression = (
+  left: o.MonkeyObject,
+  operator: string,
+  right: o.MonkeyObject
+): o.MonkeyObject => {
+  const evaluator = BINARY_OPERATORS.get(operator);
+  if (!evaluator) {
+    return NULL;
+  }
+  return evaluator(left, right);
 };
 
 export default monkeyEval;
--- test/evaluator.test.ts
+++ test/evaluator.test.ts
@@ -3,9 +3,12 @@ import Parser from "../src/parser";
 import monkeyEval from "../src/evaluator";
 import * as o from "../src/object";
 
+type IntegerCase = [string, number];
+type BooleanCase = [string, boolean];
+
 describe("monkeyEval", () => {
-  it("should evaluate plain integer literals", () => {
-    const cases: [string, number][] = [
+  it("should evaluate integer literals as themselves", () => {
+    const cases: IntegerCase[] = [
       ["5", 5],
       ["10", 10],
     ];
@@ -14,6 +17,88 @@ describe("monkeyEval", () => {
       const result = testEval(input);
 
       checkIntegerObject(result, output);
+    });
+  });
+
+  it("should evaluate boolean literals as themselves", () => {
+    const cases: BooleanCase[] = [
+      ["true", true],
+      ["false", false],
+    ];
+
+    cases.forEach(([input, output]) => {
+      const result = testEval(input);
+
+      checkBooleanObject(result, output);
+    });
+  });
+
+  it("should evaluate the prefix bang operator as logical NOT", () => {
+    const cases: BooleanCase[] = [
+      ["!true", false],
+      ["!false", true],
+      ["!5", false],
+      ["!!true", true],
+      ["!!false", false],
+      ["!!5", true],
+    ];
+
+    cases.forEach(([input, output]) => {
+      const result = testEval(input);
+
+      checkBooleanObject(result, output);
+    });
+  });
+
+  it("should evaluate arithmetic operators correctly", () => {
+    const cases: IntegerCase[] = [
+      ["-5", -5],
+      ["-10", -10],
+      ["5 + 5 + 5 + 5 - 10", 10],
+      ["2 * 2 * 2 * 2 * 2", 32],
+      ["-50 + 100 + -50", 0],
+      ["5 * 2 + 10", 20],
+      ["5 + 2 * 10", 25],
+      ["20 + 2 * -10", 0],
+      ["50 / 2 * 2 + 10", 60],
+      ["2 * (5 + 10)", 30],
+      ["3 * 3 * 3 + 10", 37],
+      ["3 * (3 * 3) + 10", 37],
+      ["(5 + 10 * 2 + 15 / 3) * 2 + -10", 50],
+    ];
+
+    cases.forEach(([input, output]) => {
+      const result = testEval(input);
+
+      checkIntegerObject(result, output);
+    });
+  });
+
+  it("should evaluate boolean operators correctly", () => {
+    const cases: BooleanCase[] = [
+      ["1 < 2", true],
+      ["1 > 2", false],
+      ["1 < 1", false],
+      ["1 > 1", false],
+      ["1 == 1", true],
+      ["1 != 1", false],
+      ["1 == 2", false],
+      ["1 != 2", true],
+      ["true == true", true],
+      ["false == false", true],
+      ["true == false", false],
+      ["true != false", true],
+      ["false != true", true],
+      ["(1 < 2) == true", true],
+      ["(1 < 2) == false", false],
+      ["(1 > 2) == true", false],
+      ["(1 > 2) == false", true],
+    ];
+
+    cases.forEach(([input, output]) => {
+      const result = testEval(input);
+
+      checkBooleanObject(result, output);
     });
   });
 });
@@ -32,4 +117,11 @@ const checkIntegerObject = (result: o.MonkeyObject, n: number) => {
   const integer = result as o.MonkeyInteger;
 
   expect(integer.value).toBe(n);
+};
+
+const checkBooleanObject = (result: o.MonkeyObject, b: boolean) => {
+  expect(result).toBeInstanceOf(o.MonkeyBoolean);
+  const boolObj = result as o.MonkeyBoolean;
+
+  expect(boolObj.value).toBe(b);
 };
