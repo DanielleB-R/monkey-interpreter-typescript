7c0472d53322bcc68a68b3149904916c686a539d
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2021-01-26 15:56:45 -0800

    Implement modify cases for all necessary types

--- src/modify-ast.ts
+++ src/modify-ast.ts
@@ -4,24 +4,89 @@ export interface ModifierFunc {
   (node: ast.Node): ast.Node;
 }
 
+const assertStatement = (node: ast.Node): ast.Statement => {
+  if (!ast.isStatement(node)) {
+    throw "modifier made statement into non-statement";
+  }
+  return node;
+};
+
+const assertExpression = (node: ast.Node): ast.Expression => {
+  if (!ast.isExpression(node)) {
+    throw "modifier made expression into non-expression";
+  }
+  return node;
+};
+
+const assertBlockStatement = (node: ast.Node): ast.BlockStatement => {
+  if (node.nodeType !== ast.NodeType.BLOCK) {
+    throw "modifier made block statement into non-block";
+  }
+  return node;
+};
+
+const assertIdentifier = (node: ast.Node): ast.Identifier => {
+  if (node.nodeType !== ast.NodeType.IDENTIFIER) {
+    throw "modifier made identifier into non-identifier";
+  }
+  return node;
+};
+
 const modify = (node: ast.Node, modifier: ModifierFunc): ast.Node => {
   switch (node.nodeType) {
     case ast.NodeType.PROGRAM:
-      node.statements = node.statements.map((stmt) => {
-        const newStatement = modify(stmt, modifier);
-        if (!ast.isStatement(newStatement)) {
-          throw "modifier made statement into non-statement";
-        }
-        return newStatement;
-      });
+    case ast.NodeType.BLOCK:
+      node.statements = node.statements.map((stmt) =>
+        assertStatement(modify(stmt, modifier))
+      );
+      break;
+    case ast.NodeType.LET:
+      node.value = assertExpression(modify(node.value, modifier));
+      break;
+    case ast.NodeType.RETURN:
+      node.returnValue = assertExpression(modify(node.returnValue, modifier));
       break;
     case ast.NodeType.EXPR_STMT:
-      const newExpression = modify(node.expression, modifier);
-      if (!ast.isExpression(newExpression)) {
-        throw "modifier made expression into non-expression";
-      }
-      node.expression = newExpression;
+      node.expression = assertExpression(modify(node.expression, modifier));
       break;
+    case ast.NodeType.INFIX:
+      node.left = assertExpression(modify(node.left, modifier));
+      node.right = assertExpression(modify(node.right, modifier));
+      break;
+    case ast.NodeType.PREFIX:
+      node.right = assertExpression(modify(node.right, modifier));
+      break;
+    case ast.NodeType.INDEX:
+      node.left = assertExpression(modify(node.left, modifier));
+      node.index = assertExpression(modify(node.index, modifier));
+      break;
+    case ast.NodeType.IF:
+      node.condition = assertExpression(modify(node.condition, modifier));
+      node.consequence = assertBlockStatement(
+        modify(node.consequence, modifier)
+      );
+      if (node.alternative) {
+        node.alternative = assertBlockStatement(
+          modify(node.alternative, modifier)
+        );
+      }
+      break;
+    case ast.NodeType.FN:
+      node.parameters = node.parameters.map((param) =>
+        assertIdentifier(modify(param, modifier))
+      );
+      node.body = assertBlockStatement(modify(node.body, modifier));
+      break;
+    case ast.NodeType.ARRAY:
+      node.elements = node.elements.map((elem) =>
+        assertExpression(modify(elem, modifier))
+      );
+      break;
+    case ast.NodeType.HASH:
+      node.pairs = node.pairs.map(([key, val]) => [
+        assertExpression(modify(key, modifier)),
+        assertExpression(modify(val, modifier)),
+      ]);
   }
   return modifier(node);
 };
--- test/modify-ast.test.ts
+++ test/modify-ast.test.ts
@@ -3,7 +3,7 @@ import * as ast from "../src/ast-json";
 import modify from "../src/modify-ast";
 
 describe("modify", () => {
-  it("should modify the ASST appropriately", () => {
+  it("should modify the AST appropriately", () => {
     const one = (): ast.Expression =>
       ast.buildInteger(token.makeToken(token.TokenType.INT, "1"));
     const two = (): ast.Expression =>
@@ -42,12 +42,188 @@ describe("modify", () => {
           ],
         },
       ],
+      [
+        {
+          nodeType: ast.NodeType.INFIX,
+          left: one(),
+          operator: "+",
+          right: two(),
+        },
+        {
+          nodeType: ast.NodeType.INFIX,
+          left: two(),
+          operator: "+",
+          right: two(),
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.INFIX,
+          left: two(),
+          operator: "+",
+          right: one(),
+        },
+        {
+          nodeType: ast.NodeType.INFIX,
+          left: two(),
+          operator: "+",
+          right: two(),
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.PREFIX,
+          operator: "-",
+          right: one(),
+        },
+        {
+          nodeType: ast.NodeType.PREFIX,
+          operator: "-",
+          right: two(),
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.INDEX,
+          left: one(),
+          index: one(),
+        },
+        {
+          nodeType: ast.NodeType.INDEX,
+          left: two(),
+          index: two(),
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.IF,
+          condition: one(),
+          consequence: {
+            nodeType: ast.NodeType.BLOCK,
+            statements: [
+              {
+                nodeType: ast.NodeType.EXPR_STMT,
+                expression: one(),
+              },
+            ],
+          },
+          alternative: {
+            nodeType: ast.NodeType.BLOCK,
+            statements: [
+              {
+                nodeType: ast.NodeType.EXPR_STMT,
+                expression: one(),
+              },
+            ],
+          },
+        },
+        {
+          nodeType: ast.NodeType.IF,
+          condition: two(),
+          consequence: {
+            nodeType: ast.NodeType.BLOCK,
+            statements: [
+              {
+                nodeType: ast.NodeType.EXPR_STMT,
+                expression: two(),
+              },
+            ],
+          },
+          alternative: {
+            nodeType: ast.NodeType.BLOCK,
+            statements: [
+              {
+                nodeType: ast.NodeType.EXPR_STMT,
+                expression: two(),
+              },
+            ],
+          },
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.RETURN,
+          returnValue: one(),
+        },
+        {
+          nodeType: ast.NodeType.RETURN,
+          returnValue: two(),
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.LET,
+          name: ast.buildIdentifier(
+            token.makeToken(token.TokenType.IDENT, "test")
+          ),
+          value: one(),
+        },
+        {
+          nodeType: ast.NodeType.LET,
+          name: ast.buildIdentifier(
+            token.makeToken(token.TokenType.IDENT, "test")
+          ),
+          value: two(),
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.FN,
+          parameters: [],
+          body: {
+            nodeType: ast.NodeType.BLOCK,
+            statements: [
+              {
+                nodeType: ast.NodeType.EXPR_STMT,
+                expression: one(),
+              },
+            ],
+          },
+        },
+        {
+          nodeType: ast.NodeType.FN,
+          parameters: [],
+          body: {
+            nodeType: ast.NodeType.BLOCK,
+            statements: [
+              {
+                nodeType: ast.NodeType.EXPR_STMT,
+                expression: two(),
+              },
+            ],
+          },
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.ARRAY,
+          elements: [one(), one()],
+        },
+        {
+          nodeType: ast.NodeType.ARRAY,
+          elements: [two(), two()],
+        },
+      ],
+      [
+        {
+          nodeType: ast.NodeType.HASH,
+          pairs: [
+            [one(), one()],
+            [one(), one()],
+          ],
+        },
+        {
+          nodeType: ast.NodeType.HASH,
+          pairs: [
+            [two(), two()],
+            [two(), two()],
+          ],
+        },
+      ],
     ];
 
     cases.forEach(([input, output]) => {
-      const modified = modify(input, oneIntoTwo);
-
-      expect(modified).toEqual(output);
+      expect(modify(input, oneIntoTwo)).toEqual(output);
     });
   });
 });
